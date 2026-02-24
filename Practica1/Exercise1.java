7b3fe863931ebf461153d4c2bb986228871a02b9
Uriel <uriel12mtzhdz@gmail.com>
2026-02-23 19:12:54 -0600

    Ejercicio 1 completo

--- Practica1/Exercise1.java
+++ Practica1/Exercise1.java
@@ -17,28 +17,29 @@ public class Exercise1 {
         System.out.println("Primo p generado: " + p);
         
         // 2. Encontrar todos los pares (a, b) que forman curvas no singulares
-        List<int[]> validCurves = findNonSingularCurves(p.intValue());
+        List<int[]> curvasValidas = buscarCurvasNoSingulares(p.intValue());
 
-        System.out.println("Se encontraron " + validCurves.size() + " curvas no singulares.");
-        System.out.println("Primeros 10 resultados (a, b):");
-        for (int i = 0; i < Math.min(10, validCurves.size()); i++) {
-            System.out.println("a: " + validCurves.get(i)[0] + ", b: " + validCurves.get(i)[1]);
+        System.out.println("Se encontraron " + curvasValidas.size() + " curvas no singulares.");
+        System.out.println("Primeros 5 resultados (a, b):");
+        for (int i = 0; i < Math.min(5, curvasValidas.size()); i++) {
+            int[] datos = curvasValidas.get(i);
+            System.out.println("a = " + datos[0] + " ; b = " + datos[1] + " ; Delta = " + datos[2]);
         }
     }
 
-    public static List<int[]> findNonSingularCurves(int p) {
-        List<int[]> pairs = new ArrayList<>();
+    public static List<int[]> buscarCurvasNoSingulares(int p) {
+        List<int[]> pares = new ArrayList<>();
 
         for (int a = 0; a < p; a++) {
             for (int b = 0; b < p; b++) {
-                // Condición: (4a^3 + 27b^2) mod p != 0
-                int discriminant = (4 * a * a * a + 27 * b * b) % p;
+                // Condicion: (4a^3 + 27b^2) mod p != 0
+                int discriminante = (4 * a * a * a + 27 * b * b) % p;
                 
-                if (discriminant != 0) {
-                    pairs.add(new int[]{a, b});
+                if (discriminante != 0) {
+                    pares.add(new int[]{a, b, discriminante});
                 }
             }
         }
-        return pairs;
+        return pares;
     }
 }
